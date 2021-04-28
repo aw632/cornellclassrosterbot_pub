0c663d315dbedeecc49fbe2d11227d657992ca92
Andrew Wang <aw632@cornell.edu>
2021-04-28 14:25:45 -0400

    fix typo

--- bot.py
+++ bot.py
@@ -253,7 +253,7 @@ async def get(ctx, dep, num):
         await ctx.send(
             "If you are reading this, it means that Easter Egg worked finally."
         )
-    elif (dep.upper() == "ANTHR") and (num == "3352"):
+    elif (dep.upper() == "ANTHR") and (num == "3552"):
         await ctx.send(
             "The class is blocked from classrosterbot. Please view it at classes.cornell.edu if you want it."
         )
